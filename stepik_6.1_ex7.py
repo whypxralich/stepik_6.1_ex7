c55346c9d768a8ff1ca4cb1e4e273c5dd9831b61
maxgliantsev <143866680+whypxralich@users.noreply.github.com>
2024-03-02 16:12:03 +0300

    Create stepik_6.1_ex7.py

---
+++ stepik_6.1_ex7.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+x = max(a, b, c, d, e)
+y = min(a, b, c, d, e)
+
+print ("Наименьшее число =", y)
+print ("Наибольшее число =", x)
